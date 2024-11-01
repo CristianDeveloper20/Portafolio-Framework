92feaee4063e3d99e9774e2fb0350254c193a190
CristianDeveloper20 <cristianfernandocruzusme@gmail.com>
2024-11-01 05:44:54 -0500

    fix: update Travis CI configuration and asset paths for improved deployment and consistency

--- src/main.js
+++ src/main.js
@@ -1,27 +1,16 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router';
-import './style.css'
+import router from './route'; // Asegúrate de que el archivo y la ruta existen
+import './style.css';
 
+// Configuración de Font Awesome (si se usa en el proyecto)
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
-// Importa Font Awesome
-import { library } from '@fortawesome/fontawesome-svg-core' /* import the fontawesome core */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome' /* import font awesome icon component */
-import { fab } from '@fortawesome/free-brands-svg-icons'
-
-
-// Añade los iconos a la librería
-library.add(fab)
-
+library.add(fab);
 
 const app = createApp(App);
-
-
-// Registra el componente font-awesome-icon
-app.component('font-awesome-icon', FontAwesomeIcon);
-
-
 app.use(router);
-
-
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.mount('#app');
